b2cce46d9dc8b4497bc10248ec26ace6af745121
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-05 14:49:34 -0600

    pushing hurtz

--- db/linkQuery.js
+++
@@ -1,9 +0,0 @@
-const knex = require('knex');
-
-function showProfile(uname) {
-  return knex('students').select().where('uname', uname)
-}
-
-module.exports = {
-  showProfile
-}
--- routes/profile.js
+++ routes/profile.js
@@ -4,13 +4,34 @@ var linkQuery = require('../db/linkQuery');
 var knex = require('../db/knex');
 
 //router mounted at localhost:3000/profile
-router.get('/student/:uname', function(req, res, next) {
-  const uname = req.params.uname
-  linkQuery.showProfile(uname)
-  .then((data) => {
-    console.log(data)
-    res.render('student-profile', {uname:data[0]} );
+
+router.get('/student/:uname', (req, res) =>{
+  var user = req.params.uname
+  knex('students').select().where('uname', user).then((data) => {
+    res.render('student-profile', {user: data[0]})
   })
+})
+
+router.post('/', function(req, res, next) {
+  const user = req.body;
+  knex('students').select().where('uname', user.uname)
+  .then((data) => {
+      res.redirect('profile/student/'+ user.uname);
+    })
 });
 
-module.exports = router
+// router.post('/', (req, res) =>{
+//   var user = req.body;
+//   pg('personal').select().where('user_name', user.user_name).then((info) => {
+//     console.log(info[0].id);
+//     bcrypt.compare(user.password, info[0].password).then((pass) => {
+//       if (pass) {
+//           res.redirect('/profile/' + user.user_name);
+//       }else{
+//
+//       }
+//     })
+//   });
+// });
+
+module.exports = router;
