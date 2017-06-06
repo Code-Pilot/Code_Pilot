be3befd82aad7213a01a930e3dee5308f230df3a
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-05 20:31:07 -0600

    db stuff

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   connection: 'postgres://localhost/code_pilot',
   },
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true'
+      client: 'postgresql',
+      connection: 'postgres://rkdbqxxccfxrdy:187768b123507782fea7e339fb62a2649060e18277d52c81c9b8bd915d957621@ec2-23-23-234-118.compute-1.amazonaws.com:5432/decnkra9fa1kv6'
+    }
   }
-}
