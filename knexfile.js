505698f87c7279fea69680edda5191253ca6ced2
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-05 18:11:42 -0600

    omg

--- knexfile.js
+++ knexfile.js
@@ -8,10 +8,6 @@ module.exports = {
   },
   production: {
       client: 'postgresql',
-      connection: {
-        database: 'postgres://rkdbqxxccfxrdy:187768b123507782fea7e339fb62a2649060e18277d52c81c9b8bd915d957621@ec2-23-23-234-118.compute-1.amazonaws.com:5432/decnkra9fa1kv6',
-        user:     'username',
-        password: 'password'
-      }
+      connection: 'postgres://rkdbqxxccfxrdy:187768b123507782fea7e339fb62a2649060e18277d52c81c9b8bd915d957621@ec2-23-23-234-118.compute-1.amazonaws.com:5432/decnkra9fa1kv6'
     }
   }
