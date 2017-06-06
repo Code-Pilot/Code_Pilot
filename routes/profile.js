5a3b8c958c75126ca117b98d5076fb25cdc75008
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-05 22:17:23 -0600

    add teachers and students

--- routes/profile.js
+++ routes/profile.js
@@ -6,32 +6,35 @@ var knex = require('../db/knex');
 //router mounted at localhost:3000/profile
 
 router.get('/student/:uname', (req, res) =>{
-  var user = req.params.uname
-  knex('students').select().where('uname', user).then((data) => {
-    res.render('student-profile', {user: data[0]})
+  var student = req.params.uname
+  knex('students').select().where('uname', student).then((data) => {
+    res.render('student-profile', {student: data[0]})
   })
 })
 
 router.post('/', function(req, res, next) {
-  const user = req.body;
-  knex('students').select().where('uname', user.uname)
+  const student = req.body;
+  knex('students').select().where('uname', student.uname)
   .then((data) => {
-      res.redirect('profile/student/'+ user.uname);
+      res.redirect('profile/student/'+ student.uname);
     })
 });
 
-// router.post('/', (req, res) =>{
-//   var user = req.body;
-//   pg('personal').select().where('user_name', user.user_name).then((info) => {
-//     console.log(info[0].id);
-//     bcrypt.compare(user.password, info[0].password).then((pass) => {
-//       if (pass) {
-//           res.redirect('/profile/' + user.user_name);
-//       }else{
-//
-//       }
-//     })
-//   });
-// });
+
+router.get('/teacher/:uname', (req, res) =>{
+  var teacher = req.params.uname
+  knex('teachers').select().where('uname', teacher).then((data) => {
+    res.render('teacher-profile', {teacher: data[0]})
+  })
+})
+
+router.post('/teacher', function(req, res, next) {
+  console.log('mehhhh');
+  const teacher = req.body;
+  knex('teachers').select().where('uname', teacher.uname)
+  .then((data) => {
+      res.redirect('/profile/teacher/'+ teacher.uname);
+    })
+});
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -6,19 +6,35 @@ var knex = require('../db/knex');
 
 //router mounted at localhost:3000/signup
 router.post('/', function(req,res,next){
-  var user = req.body;
+  var student = req.body;
   knex('students').insert({
-    uname: user.uname,
-    pword: user.pword,
-    fname: user.fname,
-    lname: user.lname,
-    email: user.email,
+    uname: student.uname,
+    pword: student.pword,
+    fname: student.fname,
+    lname: student.lname,
+    email: student.email,
     bio:'',
     priorKnowledge: '',
     toKnow: ''
   })
   .then(()=>{
-    res.redirect('/profile/student/' + user.uname)
+    res.redirect('/profile/student/' + student.uname)
+  })
+})
+
+router.post('/teacher', function(req, res, next){
+  var teacher = req.body;
+  knex('teachers').insert({
+    uname: teacher.uname,
+    pword: teacher.pword,
+    fname: teacher.fname,
+    lname: teacher.lname,
+    email: teacher.email,
+    bio:'',
+    skills: ''
+  })
+  .then(()=>{
+    res.redirect('/profile/teacher/' + teacher.uname)
   })
 })
 
