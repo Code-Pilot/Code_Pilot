286ce6b6d9a4e28b5d84f131b9387defbda553df
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-05 23:40:21 -0600

    wow

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,13 @@
-// Update with your config settings.
+
 
 module.exports = {
 
   development: {
-  client: 'postgresql',
-  connection: 'postgres://localhost/code_pilot',
+    client: 'postgresql',
+    connection: 'postgres://localhost/code_pilot'
   },
   production: {
-      client: 'postgresql',
-      connection: {
-        database: 'postgres://rkdbqxxccfxrdy:187768b123507782fea7e339fb62a2649060e18277d52c81c9b8bd915d957621@ec2-23-23-234-118.compute-1.amazonaws.com:5432/decnkra9fa1kv6'
-      }
-    }
+    client: 'postgresql',
+    connection: 'postgres://rkdbqxxccfxrdy:187768b123507782fea7e339fb62a2649060e18277d52c81c9b8bd915d957621@ec2-23-23-234-118.compute-1.amazonaws.com:5432/decnkra9fa1kv6'
   }
+}
