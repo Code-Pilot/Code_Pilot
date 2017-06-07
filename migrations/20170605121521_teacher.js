39606fd31eadb41a9ed1645e1498164b841805da
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-07 11:20:27 -0600

    om

--- migrations/20170605121521_teacher.js
+++ migrations/20170605121521_teacher.js
@@ -7,8 +7,13 @@ exports.up = function(knex, Promise) {
     table.string('fname').notNullable()
     table.string('lname').notNullable()
     table.string('email').notNullable()
-    table.text('bio')
     table.text('skills')
+    table.string('userType').defaultTo('teacher')
+    table.text('bio')
+    table.string('lang1')
+    table.string('lang2')
+    table.string('lang3')
+    table.boolean('isOnline').defaultTo(false)
   })
 };
 
