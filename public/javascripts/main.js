4c4b98d2d205b67b5eb5efdbdf508fa94b3ff941
Brent Thomas <brentthomas@Brents-MacBook-Air.local>
2017-06-08 23:30:06 -0600

    almost private chat

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -37,6 +37,50 @@ $(document).ready(function() {
     })
   });
 
+  // Private Connection
+
+
+
+  // $(function() {
+  //   var socket1 = io.('/Pair-Program');
+  //   var $messageForm = $('#messageForm1');
+  //   var $message = $('#message1');
+  //   var $chat = $('#chat1');
+  //   var $messageArea = $('#messageArea1');
+  //   var $userFormArea = $('#userFormArea1')
+  //   var $userForm = $('#userForm1')
+  //   var $users = $('#users1')
+  //   var $username = $('#username1')
+  //   $messageForm.submit(function(e) {
+  //     e.preventDefault();
+  //     socket.emit('send message1', $message.val());
+  //     $message.val('');
+  //   })
+  //   socket.on('new message1', function(data) {
+  //     $chat.append('<div class="well"><strong>'+data.user+'</strong>: '+data.msg+'</div>')
+  //   })
+  //   $userForm.submit(function(e) {
+  //     e.preventDefault();
+  //     socket.emit('new user1', $username.val(), function(data){
+  //       if(data) {
+  //         $userFormArea.hide();
+  //         $messageArea.show();
+  //       }
+  //     });
+  //     $username.val('');
+  //   });
+  //   socket.on('get users1', function(data){
+  //     var html = '';
+  //     for(i = 0; i < data.length; i++) {
+  //       html += '<li class="list-group-item">'+data[i]+'</li>';
+  //     }
+  //     $users.html(html)
+  //   })
+  // });
+
+
+// End of Private Connection
+
   $('.enterChat').click(function() {
     $('#messageArea').toggleClass('hide')
   })
@@ -55,10 +99,6 @@ $(document).ready(function() {
     $('.login-modal-button').replaceWith('<a href="/"><button>Back</button></a>')
   })
 
-
-    $('.onlineTeacher').click(function() {
-      $('.teacher-modal').toggleClass('hide')
-    })
 
 
 
